b04afe3b266bd3b8472228a5fb73f75d00c138d9
Ahmet Turkmen <f.ahmet.turkmen@icloud.com>
2017-06-07 17:22:07 +0300

    // middleware next(); added

--- server.js
+++ server.js
@@ -17,6 +17,13 @@ var app = express();
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
+app.use(function(req,res,next){
+console.log('url ...:'+req.originalUrl);
+console.log('time...: '+Date.now());
+next();
+
+})
+
 app.use('/electronic', routeElectronic);
 
 app.listen(8000);
